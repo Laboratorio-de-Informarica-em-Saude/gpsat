93a8c9e70335c3d0e1fea8fb73cb0f2e8a87b8db
Macário Soares <macario.junior@gmail.com>
2017-09-02 11:40:01 -0300

    Starting api commit

---
+++ gpsatAPI/app/api/__init__.py
@@ -0,0 +1,12 @@
+"""Blueprint api for gpsat."""
+from flask import Blueprint
+
+api = Blueprint('api', __name__)
+
+from . import form, errors
+
+
+def create_app(config_name):
+    """Create app for api gpsat."""
+    from .api import api as api_1_blueprint
+    app.register_blueprint(api_1_blueprint, url_prefix='/api/v1')

cc93de1f290b6a647f9d68c1e5f8523093bab15d
Macário Soares <macario.junior@gmail.com>
2017-11-01 15:21:00 -0300

    Reinicializing React.js

--- gpsatWeb/gpsat/src/App.js
+++ gpsatWeb/gpsat/src/App.js
@@ -8,11 +8,9 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>GPSAT - Análises em pacientes amputados.</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }

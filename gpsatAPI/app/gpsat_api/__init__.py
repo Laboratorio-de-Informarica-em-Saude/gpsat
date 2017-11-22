afe895ba6a18e7c577627b0323fd49413cb466f6
Macário Soares <macario.junior@gmail.com>
2017-11-21 23:48:00 -0300

    Applying CORS savings

--- gpsatAPI/app/gpsat_api/__init__.py
+++ gpsatAPI/app/gpsat_api/__init__.py
@@ -1,6 +1,7 @@
 from config import config
 from flask import Flask
 from pymongo import MongoClient
+from flask_cors import CORS
 
 
 def create_app(config_name):
@@ -9,6 +10,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     from .api import main_blueprint
     app.register_blueprint(main_blueprint)
+    CORS(app, resources=r'/api/*', allow_headers='Content-Type')
     return app
 
 
